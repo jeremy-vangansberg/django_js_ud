441cd3c2288a8517b04e38decaab285185bed348
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2022-11-28 07:47:20 +0100

    cards

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,26 +1,40 @@
 
 const helloWorldBox = document.getElementsByClassName('hello-world')
-
-helloWorldBox[0].textContent = 'test'
-
-$.ajax({
-    type: 'GET',
-    url: '/hello-world/',
-    success: function (response) {
-        console.log('success', response)
-    },
-    error: function (response) {
-        console.log('error', response)
-    }
-})
+const postsBox = document.getElementById('posts')
+const spinnerBox = document.getElementById('spinner-box')
 
 $.ajax({
     type: 'GET',
     url: '/data/',
     success: function (response) {
         console.log('success', response)
-        const data = JSON.parse(response.data)
-        console.log(data)
+        const data = response.data
+        setTimeout(() => {
+            spinnerBox.classList.add('not-visible')
+            data.forEach(el => {
+                postsBox.innerHTML += `
+                <div class="card mb-2">
+                    <div class="card-body">
+                        <h5 class="card-title">${el.title}</h5>
+                        <p class="card-text">${el.body}</p>
+                        
+                    </div>
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-1">
+                                <a href="#" class="btn btn-primary">Detail</a>
+                            </div>
+                            <div class="col-1">
+                                <a href="#" class="btn btn-primary">Like</a>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            `
+            });
+        }, 100)
+
     },
     error: function (response) {
         console.log('error', response)
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
-from django.core import serializers
-
 
 def post_list_and_create(request):
     qs = Post.objects.all()
@@ -14,5 +12,13 @@ def hello_world(request):
 
 def load_post_data_view(request):
     qs = Post.objects.all()
-    data = serializers.serialize('json', qs)
+    data = []
+    for obj in qs:
+        item = {
+            'id': obj.id,
+            'title': obj.title,
+            'body': obj.body,
+            'author': obj.author.user.username,
+        }
+        data.append(item)
     return JsonResponse({'data' : data})    
